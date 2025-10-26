28e13c59957d428e270efc09dc8646e2c4f8057e
Vinol Noronha <vinolnoronha7@gmail.com>
2025-10-26 16:38:52 +0530

    frontend completely done

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,9 @@
 import { Fjalla_One, Noto_Sans_Javanese } from "next/font/google";
 import Image from "next/image";
 import Logo from "@/public/invoicelylogo.png";
+import Ocrimage from "@/public/ocrfeature.png";
+import Chartsimage from "@/public/charts.png";
+import Dashboardimage from "@/public/dashboard.png";
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
@@ -70,9 +73,69 @@ export default function Home() {
             </div>
           </section>
           <section className=" h-full w-full bg-yellow-30 flex flex-col sm:flex-row gap-6 sm:gap-20 justify-center items-center px-4 py-10 sm:py-0">
-            <div className="h-60 w-full sm:h-70 sm:w-75 bg-gray-100 rounded-md"></div>
-            <div className="h-60 w-full sm:h-70 sm:w-75 bg-gray-100 rounded-md"></div>
-            <div className="h-60 w-full sm:h-70 sm:w-75 bg-gray-100 rounded-md"></div>
+            <div className="h-60 w-full sm:h-70 sm:w-75 bg-white rounded-md shadow-lg border border-blue-100">
+              <div className="h-full w-full flex flex-col items-center py-3">
+                <div className="h-[70%] w-[93%] overflow-hidden relative rounded-sm border-2 border-blue-200">
+                  <Image
+                    className="object-cover"
+                    alt="OCR feature"
+                    src={Ocrimage}
+                    fill
+                    sizes="(max-width: 640px) 100vw, 18.75rem"
+                  />
+                </div>
+
+                <div className="mt-3 rounded-sm h-[20%] w-[93%] bg-blue-50 border border-blue-200 flex items-center justify-center">
+                  <text
+                    className={`${noto_sans_javanese.className} text-xs px-4 text-center font-semibold text-blue-700`}
+                  >
+                    AI-Powered OCR Invoice Extraction
+                  </text>
+                </div>
+              </div>
+            </div>
+            <div className="h-60 w-full sm:h-70 sm:w-75 bg-white rounded-md shadow-lg border border-blue-100">
+              <div className="h-full w-full flex flex-col items-center py-3">
+                <div className="h-[70%] w-[93%] overflow-hidden relative rounded-sm border-2 border-blue-200">
+                  <Image
+                    className="object-cover"
+                    alt="OCR feature"
+                    src={Chartsimage}
+                    fill
+                    sizes="(max-width: 640px) 100vw, 18.75rem"
+                  />
+                </div>
+
+                <div className="mt-3 rounded-sm h-[20%] w-[93%] bg-blue-50 border border-blue-200 flex items-center justify-center">
+                  <text
+                    className={`${noto_sans_javanese.className} text-xs px-4 text-center font-semibold text-blue-700`}
+                  >
+                    Real-Time Revenue and GST Insights
+                  </text>
+                </div>
+              </div>
+            </div>
+            <div className="h-60 w-full sm:h-70 sm:w-75 bg-white rounded-md shadow-lg border border-blue-100">
+              <div className="h-full w-full flex flex-col items-center py-3">
+                <div className="h-[70%] w-[93%] overflow-hidden relative rounded-sm border-2 border-blue-200">
+                  <Image
+                    className="object-cover"
+                    alt="OCR feature"
+                    src={Dashboardimage}
+                    fill
+                    sizes="(max-width: 640px) 100vw, 18.75rem"
+                  />
+                </div>
+
+                <div className="mt-3 rounded-sm h-[20%] w-[93%] bg-blue-50 border border-blue-200 flex items-center justify-center">
+                  <text
+                    className={`${noto_sans_javanese.className} text-xs px-4 text-center font-semibold text-blue-700`}
+                  >
+                    Smart Invoice Tracking
+                  </text>
+                </div>
+              </div>
+            </div>
           </section>
         </section>
       </main>
